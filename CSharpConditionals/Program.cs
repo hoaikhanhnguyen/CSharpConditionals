8965cbddf8363ab3353208044b29348c90fb428a
Khanh Nguyen <con.nguyen.86@gmail.com>
2017-11-07 20:37:08 -0800

    complete

--- CSharpConditionals/Program.cs
+++ CSharpConditionals/Program.cs
@@ -10,6 +10,49 @@ namespace CSharpConditionals
     {
         static void Main(string[] args)
         {
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning.");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's afternoon.");
+            }
+            else
+            {
+                Console.WriteLine("It's evening.");
+            }
+
+            bool isGoldCustomer = true;
+
+            //if (isGoldCustomer)
+            //    price = 19.95f;
+            //else
+            //    price = 29.95f;
+
+            float price = (isGoldCustomer) ? 19.95f : 29.95f;
+            //ternarys in c#!!!
+
+            Console.WriteLine(price);
+
+            var season = Season.Autumn;
+
+            switch(season)
+            {
+                case Season.Autumn:
+                    Console.WriteLine("It's autumn and a beautiful season.");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("It's perfect to go to the beach.");
+                    break;
+
+                default:
+                    Console.WriteLine("I don't understand that season!");
+                    break;
+                
+            }
         }
     }
 }
